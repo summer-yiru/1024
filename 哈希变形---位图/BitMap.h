2e34d5a706a1e9f00d80e496404ee71fd4ba1e65
summer-yiru <2448826571@qq.com>
2018-06-04 15:43:55 +0800

    哈希变形--位图

---
+++ 哈希变形---位图/BitMap.h
@@ -0,0 +1,13 @@
+﻿typedef struct BitMap
+{
+	int * Bit_bset;//位的集合
+	int _capacity;
+	int _size;//有效bit位的个数
+}BitMap;
+void BitMapInit(BitMap * bmp,int capacity);
+void BitMapSet_zero(BitMap * bmp,int pos);
+void BitMapSet_one(BitMap *bmp,int pos);
+int  BitMapTest(BitMap *bmp, int pos);//测试bit位为1还是为0;
+int BitMapSize(BitMap *bmp);
+int BitMapCount(BitMap *bmp);
+void BitMapDestroy(BitMap *bmp);
---
+++ 哈希变形---位图/test.c
@@ -0,0 +1,25 @@
+﻿#include "BitMap.h"
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	BitMap bmp;
+	int i = 0;
+	int len = 0;
+	int array[] = {1,3,7,4,12,16,19,13,22,18};
+	BitMapInit(&bmp, 24);
+	len = sizeof(array) / sizeof(array[0]);
+	for ( i = 0; i < len; i++)
+	{
+		BitMapSet_one(&bmp, array[i]);
+	}
+	if (BitMapTest(&bmp, 22)){
+		printf("存在\n");
+	}
+	if (BitMapTest(&bmp, 20)){
+		printf("不存在\n");
+	}
+	printf("%d\n", BitMapSize(&bmp));
+	printf("%d\n", BitMapCount(&bmp));
+	system("pause");
+	return 0;
+}
