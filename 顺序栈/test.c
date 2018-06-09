f3eaddc5ff87538477b9b133ca9155384cf19a9f
summer-yiru <2448826571@qq.com>
2018-06-09 23:31:12 +0800

    顺序栈

---
+++ 顺序栈/test.c
@@ -0,0 +1,19 @@
+﻿#include "stack.h"
+#include <stdlib.h>
+#include<stdio.h>
+SeqStack s;
+int main(){
+	StackInit(&s);
+	StackPush(&s, 3);
+	StackPush(&s, 5);
+	StackPush(&s, 7);
+	StackPush(&s, 9);
+	StackPush(&s, 0);
+    StackPop(&s);
+	StackPop(&s);
+	StackPop(&s);
+	printf("%d ", StackTop(&s));
+	printf("%d ", StackSize(&s));
+    system("pause");
+	return 0;
+}
