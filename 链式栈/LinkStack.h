36b2bb95b2922d9cb528e90c73c291fc96cd4dcd
summer-yiru <2448826571@qq.com>
2018-08-01 22:39:40 +0800

    链式栈

---
+++ 链式栈/LinkStack.h
@@ -0,0 +1,37 @@
+﻿#ifndef __STACK_H__
+#define __STACK_H__
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<malloc.h>
+extern struct BinTreeNode;
+typedef  struct BinTreeNode* DataType;
+typedef struct StackNode
+{
+	DataType data;
+	struct StackNode * next;
+}StackNode,*pStackNode;
+typedef struct LinkStack{
+	StackNode *top;//栈顶指针
+	int stackSize;//栈的当前容量
+}LinkStack,*pLinkStack;
+void StackInit(LinkStack *s);
+
+// 入栈
+void StackPush(pLinkStack s, DataType data);
+
+// 出栈
+void StackPop(pLinkStack s);
+
+// 获取栈顶元素
+DataType StackTop(pLinkStack s);
+
+// 有效元素的个数
+int StackSize(pLinkStack s);
+
+// 检测栈是否为空
+int StackEmpty(pLinkStack s);
+//打印
+void PrintfLinkStack(LinkStack s);
+
+#endif
---
+++ 链式栈/test.c
@@ -0,0 +1,22 @@
+﻿#include "LinkStack.h"
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	LinkStack s;
+	int ret = 0;
+	StackInit(&s);
+	StackPush(&s,4);
+	StackPush(&s, 5);
+	StackPush(&s, 6);
+	StackPush(&s, 7);
+	StackPush(&s, 8);
+	StackPush(&s, 9);
+	StackPop(&s);
+	StackPop(&s);
+	//ret = StackTop(&s);
+	ret = StackSize(&s);
+	printf("%d\n", ret);
+	PrintfLinkStack(s);
+	system("pause");
+	return 0;
+}
